34681cbe36c6facd6bd3fbdac6d1259f2d8bcd2c
Chris Yang <cyang287@yahoo.com>
2018-01-22 12:23:45 -0500

    Items.js add

---
+++ scripts/Items.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Item = (function (){
+  const validateName = function (name) {
+    if (!name) {
+      Error('Name does not exist');
+    }
+  };
+
+  const create = function (name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  };
+
+  return {
+    validateName,
+    create
+  };
+}() );
